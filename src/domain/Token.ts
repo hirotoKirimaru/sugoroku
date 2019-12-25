ad73a438ca500427b2f90fff84443fced4ce868c
nainaistar <nainaistar@gmail.com>
2019-12-25 23:15:49 +0900

    [Add] 駒はEntlityなので一意性のIDを付与。またコンストラクタも定義

--- src/domain/Token.ts
+++ src/domain/Token.ts
@@ -4,10 +4,12 @@
 import Square from "@/domain/Square";
 
 export default class Token {
+  private id: string;
   /** 現在地 */
   private currentSquare: Square;
 
-  constructor(currentSquare: Square) {
-    this.currentSquare = currentSquare;
+  constructor(id: string, square ?: Square) {
+    this.id = id;
+    this.currentSquare = square || Square.init();
   }
 }
