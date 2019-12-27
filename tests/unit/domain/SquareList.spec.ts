51ffbd4a8cf653cce9e79834bab6f8daed8c73a0
nainaistar <nainaistar@gmail.com>
2019-12-27 23:33:24 +0900

    [Refactor]

--- tests/unit/domain/SquareList.spec.ts
+++ tests/unit/domain/SquareList.spec.ts
@@ -1,6 +1,15 @@
 import SquareList from '@/domain/SquareList';
 import Square from '@/domain/Square';
 
+let START: Square = new Square('-', '0', 'スタート');
+let LAWYER: Square = new Square('0', '101', '弁護士に就職マス');
+let IDOL: Square = new Square('101', '102', 'アイドルに就職マス');
+let PART: Square = new Square('102', '103', 'フリーターマス');
+let OFFICE_WORKER: Square = new Square('0', '201', '会社員に就職マス');
+let PAYDAY1: Square = new Square('103', '301', '給料日');
+let PAYDAY2: Square = new Square('201', '301', '給料日');
+let END: Square = new Square('301', '900', 'ゴール');
+
 function extracted() {
   const squareList: SquareList = new SquareList();
 
@@ -16,21 +25,6 @@ function extracted() {
 }
 
 describe('マス一覧', () => {
-  // it('初期化はArrayの初期', () => {
-  //   let squareList: SquareList = new SquareList();
-  //
-  //   squareList.add(START);
-  //   squareList.add(LAWYER);
-  //   squareList.add(IDOL);
-  //   squareList.add(PART);
-  //   squareList.add(OFFICE_WORKER);
-  //   squareList.add(PAYDAY1);
-  //   squareList.add(PAYDAY2);
-  //   squareList.add(END);
-  //
-  //   squareList.move(START, END);
-  // });
-
   it('初期位置の次は、弁護士マスか会社員マス', () => {
     const squareList: SquareList = extracted();
 
@@ -48,12 +42,3 @@ describe('マス一覧', () => {
     expect(squareList.nextList(OFFICE_WORKER)[0].position).toEqual(PAYDAY2.position);
   });
 });
-
-let START: Square = new Square('-', '0', 'スタート');
-let LAWYER: Square = new Square('0', '101', '弁護士に就職マス');
-let IDOL: Square = new Square('101', '102', 'アイドルに就職マス');
-let PART: Square = new Square('102', '103', 'フリーターマス');
-let OFFICE_WORKER: Square = new Square('0', '201', '会社員に就職マス');
-let PAYDAY1: Square = new Square('103', '301', '給料日');
-let PAYDAY2: Square = new Square('201', '301', '給料日');
-let END: Square = new Square('301', '900', 'ゴール');
