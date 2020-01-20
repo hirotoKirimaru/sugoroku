fdd5963dac006800330f079c0524316bcd0cd822
nainaistar <nainaistar@gmail.com>
2020-01-21 00:30:25 +0900

    [Add] ぐちゃぐちゃするが一応描画可能。

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -15,25 +15,25 @@ export default class Board extends Vue {
   @Prop({ default: null })
   squareList!: SquareList;
 
-  mounted(): void{
-    this.diagram = new go.Diagram('board');
+  nodeDataArray: Array<any> = [];
 
+  linkDataArray: Array<any> = [];
+
+  mounted(): void{
+    this.setNodeDataAndLinkData();
+
+    this.diagram = new go.Diagram('board');
     this.diagram.model = new go.GraphLinksModel(
-      [ // a JavaScript Array of JavaScript objects, one per node;
-        // the "color" property is added specifically for this app
-        { key: 'Alpha', color: 'lightblue' },
-        { key: 'Beta', color: 'orange' },
-        { key: 'Gamma', color: 'lightgreen' },
-        { key: 'Delta', color: 'pink' },
-      ],
-      [ // a JavaScript Array of JavaScript objects, one per link
-        { from: 'Alpha', to: 'Beta' },
-        { from: 'Alpha', to: 'Gamma' },
-        { from: 'Beta', to: 'Beta' },
-        { from: 'Gamma', to: 'Delta' },
-        { from: 'Delta', to: 'Alpha' },
-      ],
+      this.nodeDataArray,
+      this.linkDataArray,
     );
+  }
+
+  setNodeDataAndLinkData() : void {
+    this.squareList.squareList.forEach((square) => {
+      this.nodeDataArray.push({ key: square.position, text: square.description });
+      this.linkDataArray.push({ from: square.prePosition, to: square.position });
+    });
   }
 }
 </script>
